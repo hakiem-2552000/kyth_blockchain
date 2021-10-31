602c3a4e467c0a7afb05ef3b6796f33d79965ee7
Ha Vu Son Kiem <sonkiema3@gmail.com>
2021-10-31 17:17:46 +0700

    add cli for transaction

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"golang_blockchain/blockchain"
 	"os"
 )
 
 func main() {
 	defer os.Exit(0)
-	chain := blockchain.InitBlockchain("ad")
-	defer chain.Database.Close()
-
-	cli := CommandLine{chain}
+	cli := CommandLine{}
 	cli.run()
 }
